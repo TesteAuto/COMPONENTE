2804657f3d2d9f5c3d6e75d4f9c882c24c379c02
TesteAuto <cebanstela@yahoo.com>
2017-03-29 20:49:01 +0300

    Componente Fisier Initial

---
+++ FrontEnd/fisiere/aplicatie/module/controller/controllerPrincipal.js
@@ -0,0 +1,7 @@
+class controllerPrincipal {
+	constructor($scope){
+		$scope.test="un mic test";
+	}
+}
+controllerPrincipal.$inject = ['$scope'];
+export default controllerPrincipal;
---
+++ FrontEnd/fisiere/aplicatie/module/introModulParticular.js
@@ -0,0 +1,5 @@
+import controllerPrincipal from './controller/controllerPrincipal.js';
+
+export default angular.module('modulParticular', [])
+.controller('controllerPrincipal', controllerPrincipal);
+
